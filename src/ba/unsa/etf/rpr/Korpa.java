848d32425ae8fd1789d9083db1192f98a3112c4f
DinijaSeferovic <dseferovic2@etf.unsa.ba>
2020-10-31 00:28:37 +0100

    Klase Korpa i Supermarket dodane

--- src/ba/unsa/etf/rpr/Korpa.java
+++ src/ba/unsa/etf/rpr/Korpa.java
@@ -1,4 +1,46 @@
 package ba.unsa.etf.rpr;
 
 public class Korpa {
+    private final Artikl[] artikli = new Artikl[50];
+    private int br=0;
+
+    public int dajUkupnuCijenuArtikala() {
+        int ukupno=0;
+        for (int i=0; i<br; i++) ukupno+=artikli[i].getCijena();
+        return ukupno;
+    }
+
+    public Artikl izbaciArtiklSaKodom(String kod) {
+
+        for (int i = 0; i < br; i++)  {
+            if (artikli[i].getKod().equals(kod)) {
+                Artikl novi = new Artikl( artikli[i].getNaziv(), artikli[i].getCijena(), artikli[i].getKod());
+                artikli[i]=null;
+                pomjeri(i);
+                return novi;
+            }
+        }
+        return null;
+    }
+
+    public void pomjeri(int pozicija) {
+        for (int i = pozicija; i < br; i++) {
+            artikli[i]=artikli[i+1];
+        }
+        artikli[br]=null;
+        br--;
+    }
+
+    public Artikl[] getArtikli(){
+        return artikli;
+    }
+
+    public boolean dodajArtikl(Artikl a){
+        if (br<50) {
+            artikli[br]=new Artikl(a.getNaziv(), a.getCijena(), a.getKod());
+            br++;
+            return true;
+        }
+        return false;
+    }
 }
--- src/ba/unsa/etf/rpr/Supermarket.java
+++ src/ba/unsa/etf/rpr/Supermarket.java
@@ -1,4 +1,44 @@
 package ba.unsa.etf.rpr;
 
 public class Supermarket {
+    private final Artikl[] artikli= new Artikl[1000];
+    private int br=0;
+
+    public void dodajArtikl(Artikl a){
+        if (br<50) {
+            artikli[br]=new Artikl(a.getNaziv(), a.getCijena(), a.getKod());
+            br++;
+
+        }
+
+    }
+
+    public void ispisArtikala(Artikl[] a) {
+
+        for (Artikl jedanArtikl : a) System.out.println(jedanArtikl.getNaziv()+" ");
+    }
+
+    public Artikl izbaciArtiklSaKodom(String kod) {
+
+        for (int i = 0; i < br; i++)  {
+            if (artikli[i].getKod().equals(kod)) {
+                Artikl novi = new Artikl( artikli[i].getNaziv(), artikli[i].getCijena(), artikli[i].getKod());
+                artikli[i]=null;
+                pomjeri(i);
+                return novi;
+            }
+        }
+        return null;
+    }
+    public void pomjeri(int pozicija) {
+        for (int i = pozicija; i < br; i++) {
+            artikli[i]=artikli[i+1];
+        }
+        artikli[br]=null;
+        br--;
+    }
+
+    public Artikl[] getArtikli() {
+        return artikli;
+    }
 }
