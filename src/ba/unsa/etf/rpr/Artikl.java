79839dcbc8a2089121e11864db0c9726eeef9f3b
DinijaSeferovic <dseferovic2@etf.unsa.ba>
2020-10-31 00:16:55 +0100

    Klasa Artikl dodana

--- src/ba/unsa/etf/rpr/Artikl.java
+++ src/ba/unsa/etf/rpr/Artikl.java
@@ -1,4 +1,25 @@
 package ba.unsa.etf.rpr;
 
 public class Artikl {
+    private String naziv;
+    private int cijena;
+    private String kod;
+
+    public Artikl(String a, int i, String s) {
+        naziv=a; cijena=i; kod=s;
+    }
+
+    public String getNaziv() {
+        return naziv;
+    }
+
+    public String getKod() {
+        return kod;
+    }
+
+    public int getCijena() {
+        return cijena;
+    }
+
+
 }
